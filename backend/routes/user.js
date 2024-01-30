b8142ce2fe3aac195480e79d3a01025eedd4b145
Harshith Muthangi <harshithm2508@gmail.com>
2024-01-30 19:29:12 +0530

    added email at username

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,7 +6,7 @@ const JWT_SECRET = require('../config')
 const { UserModel } = require('../database')
 
 const signUpSchema = zod.object({
-    username : zod.string(),
+    username : zod.string().email(),
     password : zod.string(),
     firstName : zod.string(),
     lastName : zod.string()
