b79ae4f875d865e70b323988fef6037f82f2e746
Harshith Muthangi <harshithm2508@gmail.com>
2024-01-31 23:19:25 +0530

    user can get their balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const { Account } = require('../database');
 const router = express.Router();
+const { authMiddleware } = require('../authMiddleware');
 
+//  An endpoint for user to get their balance.
+router.get('/balance',authMiddleware,async (req,res)=>{
+    const account = await Account.findOne({
+        userId : req.userId
+    });
 
-module.exports = router;
+    req.json({
+        balance : account.balance
+    })
+})
